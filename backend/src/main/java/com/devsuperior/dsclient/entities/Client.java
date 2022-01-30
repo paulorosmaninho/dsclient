56e1a6cc357ba9765a16f089272ca6913b32b6aa
Paulo Rosmaninho <paulo.rosmaninho@gmail.com>
2022-01-30 11:37:43 -0300

    Entity Client

--- backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -3,17 +3,163 @@ package com.devsuperior.dsclient.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
 public class Client implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_client")
 	private Long id;
+	
+	@Column(name = "nm_client")
 	private String name;
+
+	@Column(name = "cd_cpf")
 	private String cpf;
+
+	@Column(name = "vl_income")
 	private Double income;
+
+	@Column(name = "dt_birth")
 	private Instant birthDate;
+
+	@Column(name = "nr_children")
 	private Integer children;
 	
-	
+	@Column(name= "ts_created_at", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
 
+	@Column(name= "ts_updated_at", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+
+	
+	public Client() {
+	}
+
+
+	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	public Double getIncome() {
+		return income;
+	}
+
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+	public Integer getChildren() {
+		return children;
+	}
+
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+	
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		this.createdAt = Instant.now();
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		this.updatedAt = Instant.now();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 }
